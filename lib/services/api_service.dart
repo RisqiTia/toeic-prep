64ab20ae5598d43dbef03b7090214eaf0c12716d
Faradhilla <dhistaameilia@gmail.com>
2026-07-30 16:43:15 +0700

    fix

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,7 +9,7 @@ class ApiService {
   // ============================================================
 
   // Hosting
-  static const String apiBaseUrl = 'https://toeic-prep.my.id/api/';
+  static const String apiBaseUrl = 'https://toeic-prep.my.id/api';
 
   static const String mediaBaseUrl = 'https://toeic-prep.my.id/asset_generator';
 
